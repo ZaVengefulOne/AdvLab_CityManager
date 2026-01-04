c32225b46f25b82d51040a148f8def3d54a6cb35
Andrey Shalomeenko <112757211+ZaVengefulOne@users.noreply.github.com>
2026-01-04 18:26:00 +0300

    Removed fullscreen feature

--- composeApp/src/jvmMain/kotlin/org/vengeful/citymanager/main.kt
+++ composeApp/src/jvmMain/kotlin/org/vengeful/citymanager/main.kt
@@ -1,25 +1,16 @@
 package org.vengeful.citymanager
 
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.WindowPlacement
-import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
-import citymanager.composeapp.generated.resources.Res
-import org.jetbrains.compose.resources.painterResource
 
 fun main() = application {
-    val windowState = WindowState(
-        placement = WindowPlacement.Fullscreen,
-        position = WindowPosition.Aligned(Alignment.Center),
-    )
+    val windowState = WindowState()
     Window(
         onCloseRequest = ::exitApplication,
         title = "Система Государственного Контроля",
         state = windowState,
-        undecorated = true,
         icon = painterResource("icon.ico")
     ) {
         App()
